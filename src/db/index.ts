55d16227203c581c58143a02d071b16ff4807f81
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2025-08-27 08:08:42 -0700

    setting up

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,6 @@
-import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 
-const db = drizzle(process.env.DATABASE_URL!);
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle({ client: sql });
+export { db };
